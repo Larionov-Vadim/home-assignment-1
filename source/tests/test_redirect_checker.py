74c1b1786c88cc33e101233abbed9fdd65666b10
Mily-V <smile_for_life@list.ru>
2015-03-16 00:40:24 +0400

    Maybe ok

--- source/tests/test_redirect_checker.py
+++ source/tests/test_redirect_checker.py
@@ -3,6 +3,7 @@ import mock
 from mock import patch
 from source.lib.utils import Config
 from source import redirect_checker
+from argparse import Namespace
 
 
 def stop_cycle(self):
@@ -11,11 +12,15 @@ def stop_cycle(self):
 def start_cycle(self):
     redirect_checker.loop = True
 
+
 config = Config()
 config.CHECK_URL = 'url'
 config.HTTP_TIMEOUT = 1
 config.WORKER_POOL_SIZE = 4
 config.SLEEP = 1
+config.LOGGING = 'logging'
+config.EXIT_CODE = 31
+
 
 class RedirectCheckerTestCase(unittest.TestCase):
 
@@ -29,5 +34,64 @@ class RedirectCheckerTestCase(unittest.TestCase):
             redirect_checker.main_loop(config)
             self.assertEqual(mock_spawn_workers.call_count, 0)
             redirect_checker.loop = True
+
+    def test_main_loop_check_network_status_ok(self):
+        mock_stop_cycle = mock.Mock(side_effect=stop_cycle)
+        mock_check_network_status = mock.Mock(return_value=True)
+        mock_spawn_workers = mock.Mock()
+        mock_active_children = mock.Mock()
+        with patch('source.redirect_checker.check_network_status', mock_check_network_status),\
+             patch('source.redirect_checker.spawn_workers', mock_spawn_workers),\
+             patch('source.redirect_checker.active_children', mock.Mock(return_value=[mock_active_children])),\
+             patch('source.redirect_checker.sleep', mock_stop_cycle):
+            redirect_checker.main_loop(config)
+            self.assertIsNot(mock_spawn_workers.call_count, 0)
+            redirect_checker.loop = True
+
+    def test_main_check_args_is_daemon_and_pidfile(self):
+
+        args = mock.MagicMock()
+        args.daemon = True
+        args.pidfile = True
+        mock_load_config_from_pyfile = mock.Mock(return_value=config)
+        mock_parse_cmd_args = mock.Mock(return_value=args)
+        mock_daemonize = mock.Mock()
+        mock_create_pidfile = mock.Mock()
+
+        with patch('source.redirect_checker.parse_cmd_args', mock_parse_cmd_args),\
+             patch('source.redirect_checker.daemonize', mock_daemonize),\
+             patch('source.redirect_checker.create_pidfile', mock_create_pidfile),\
+             patch('source.redirect_checker.load_config_from_pyfile', mock_load_config_from_pyfile),\
+             patch('source.redirect_checker.dictConfig', mock.Mock()),\
+             patch('source.redirect_checker.main_loop', mock.Mock()),\
+             patch('source.redirect_checker.os.path.realpath', mock.Mock()),\
+             patch('source.redirect_checker.os.path.expanduser', mock.Mock()):
+            return_exitcode = redirect_checker.main(args)
+            self.assertEqual(return_exitcode, config.EXIT_CODE)
+            self.assertTrue(mock_daemonize.assert_called)
+            self.assertTrue(mock_create_pidfile.assert_called)
+
+    def test_main_check_args_is_not_daemon_and_pidfile(self):
+
+        args = mock.MagicMock()
+        args.daemon = False
+        args.pidfile = False
+        mock_load_config_from_pyfile = mock.Mock(return_value=config)
+        mock_parse_cmd_args = mock.Mock(return_value=args)
+        mock_daemonize = mock.Mock()
+        mock_create_pidfile = mock.Mock()
+
+        with patch('source.redirect_checker.parse_cmd_args', mock_parse_cmd_args),\
+             patch('source.redirect_checker.daemonize', mock_daemonize),\
+             patch('source.redirect_checker.create_pidfile', mock_create_pidfile),\
+             patch('source.redirect_checker.load_config_from_pyfile', mock_load_config_from_pyfile),\
+             patch('source.redirect_checker.dictConfig', mock.Mock()),\
+             patch('source.redirect_checker.main_loop', mock.Mock()),\
+             patch('source.redirect_checker.os.path.realpath', mock.Mock()),\
+             patch('source.redirect_checker.os.path.expanduser', mock.Mock()):
+            return_exitcode = redirect_checker.main(args)
+            self.assertEqual(return_exitcode, config.EXIT_CODE)
+            self.assertEqual(mock_daemonize.call_count, 0)
+            self.assertEqual(mock_create_pidfile.call_count, 0)
 
 
