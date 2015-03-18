1cf6daac00147c2f652cc71fe6181e37256bd5cc
Mily-V <smile_for_life@list.ru>
2015-03-18 05:38:52 +0400

    test worker 100% coverage

--- source/tests/test_lib_worker.py
+++ source/tests/test_lib_worker.py
@@ -2,8 +2,7 @@ import unittest
 import mock
 from mock import patch
 from source.lib import worker
-from source.lib.utils import Config
-
+from tarantool.error import DatabaseError
 
 
 class WorkerTestCase(unittest.TestCase):
@@ -68,7 +67,6 @@ class WorkerTestCase(unittest.TestCase):
             result = worker.get_redirect_history_from_task(mock_task, timeout)
             self.assertEqual(result, waiting_result)
 
-
     def test_get_redirect_history_from_task_is_recheck_and_suspicious_in_data(self):
         timeout = 1
         url = 'url'
@@ -92,61 +90,88 @@ class WorkerTestCase(unittest.TestCase):
             result = worker.get_redirect_history_from_task(mock_task, timeout)
             self.assertEqual(result, waiting_result)
 
+
+    # def test_worker_bad_pid(self):
+    #     config = mock.MagicMock()
+    #     pid = 'Mily-V'
+    #     self.assertRaises(Exception, worker.worker(config, pid))
+
     def test_worker_task_path_is_not_exists(self):
         config = mock.MagicMock()
         pid= 31
-        path = False
         mock_get_tube = mock.MagicMock()
         mock_get_redirect_history_from_task = mock.Mock()
-
-        with patch('source.lib.worker.os.path.exists', mock.Mock(return_value=path)),\
-             patch('source.lib.worker.get_tube', mock_get_tube ),\
+        with patch('source.lib.worker.os.path.exists', mock.Mock(side_effect=[False, False])),\
+             patch('source.lib.worker.get_tube', mock_get_tube),\
              patch('source.lib.worker.get_redirect_history_from_task', mock_get_redirect_history_from_task):
             worker.worker(config, pid)
             self.assertEqual(mock_get_redirect_history_from_task.call_count, 0)
 
     def test_worker_bad_task(self):
         config = mock.MagicMock()
-        pid= 31
-        path = True
+        pid = 31
         tube = mock.MagicMock()
-        tube.take = mock.Mock(return_value=None)
+        tube.take = mock.Mock(return_value=False)
         mock_get_tube = mock.Mock(return_value=tube)
         mock_get_redirect_history_from_task = mock.Mock()
-
         with patch('source.lib.worker.os.path.exists', mock.Mock(side_effect=[True, False])),\
              patch('source.lib.worker.get_tube', mock_get_tube),\
              patch('source.lib.worker.get_redirect_history_from_task', mock_get_redirect_history_from_task):
             worker.worker(config, pid)
             self.assertEqual(mock_get_redirect_history_from_task.call_count, 0)
 
-    def test_worker_task_ok_and_result_bad(self):
+    def test_worker_result_bad(self):
+        config = mock.MagicMock()
+        pid = 31
+        mock_input_tube = mock.MagicMock()
+        mock_output_tube = mock.MagicMock()
+        mock_get_redirect_history_from_task = mock.Mock(return_value=False)
+        with patch('source.lib.worker.os.path.exists', mock.Mock(side_effect=[True, False])),\
+             patch('source.lib.worker.get_tube', mock.Mock(side_effect=[mock_input_tube, mock_output_tube])),\
+             patch('source.lib.worker.get_redirect_history_from_task', mock_get_redirect_history_from_task):
+            worker.worker(config, pid)
+            self.assertEqual(mock_input_tube.put.call_count, 0)
+            self.assertEqual(mock_output_tube.put.call_count, 0)
+
+    def test_worker_is_input_bad(self):
         config = mock.MagicMock()
         pid= 31
-        # path = True
         mock_input_tube = mock.MagicMock()
         mock_output_tube = mock.MagicMock()
         mock_get_redirect_history_from_task = mock.Mock(return_value=[False, 'data'])
-
         with patch('source.lib.worker.os.path.exists', mock.Mock(side_effect=[True, False])),\
              patch('source.lib.worker.get_tube', mock.Mock(side_effect=[mock_input_tube, mock_output_tube])),\
              patch('source.lib.worker.get_redirect_history_from_task', mock_get_redirect_history_from_task):
             worker.worker(config, pid)
-            self.assertIsNot(mock_get_redirect_history_from_task.call_count, 0)
+            self.assertEqual(mock_input_tube.put.call_count, 0)
+            self.assertIsNot(mock_output_tube.put.call_count, 0)
 
-    def test_worker_task_ok_and_result_ok(self):
+    def test_worker_is_input_ok(self):
         config = mock.MagicMock()
         pid= 31
-        # path = True
         mock_input_tube = mock.MagicMock()
         mock_output_tube = mock.MagicMock()
         mock_get_redirect_history_from_task = mock.Mock(return_value=[True, 'data'])
-
         with patch('source.lib.worker.os.path.exists', mock.Mock(side_effect=[True, False])),\
              patch('source.lib.worker.get_tube', mock.Mock(side_effect=[mock_input_tube, mock_output_tube])),\
              patch('source.lib.worker.get_redirect_history_from_task', mock_get_redirect_history_from_task):
             worker.worker(config, pid)
-            self.assertIsNot(mock_get_redirect_history_from_task.call_count, 0)
+            self.assertIsNot(mock_input_tube.put.call_count, 0)
+            self.assertEqual(mock_output_tube.put.call_count, 0)
 
+    def test_worker_task_with_DataBaseException(self):
+        config = mock.MagicMock()
+        pid = 31
+        task = mock.Mock()
+        mock_input_tube = mock.MagicMock()
+        mock_output_tube = mock.MagicMock()
+        mock_input_tube.take = mock.Mock(return_value=task)
+        task.ack = mock.Mock(side_effect=DatabaseError)
+        mock_get_redirect_history_from_task = mock.Mock(return_value=False)
+        with patch('source.lib.worker.os.path.exists', mock.Mock(side_effect=[True, False])),\
+             patch('source.lib.worker.get_tube', mock.Mock(side_effect=[mock_input_tube, mock_output_tube])),\
+             patch('source.lib.worker.get_redirect_history_from_task', mock_get_redirect_history_from_task):
+            worker.worker(config, pid)
+            self.assertRaises(DatabaseError)
 
 
